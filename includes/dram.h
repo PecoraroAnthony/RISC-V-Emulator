7cd7c59080602a8a7cf478dcdf456300d3f87bee
Anthony Pecoraro <ajpecoraro24@gmail.com>
2024-03-23 21:30:01 -0700

    DRAM and IO BUS Initial Construction

--- includes/dram.h
+++ includes/dram.h
@@ -1,4 +1,8 @@
 // dram.h
+// 64bit Vars
+// Memory-mapped I/O as per RISCV Architecture Convention
+// - lower addresses used for I/O ports 
+// - dram mem starts at  0x800000000 = DRAM_BASE
 
 #define DRAM_SIZE 1024*1024*1     // 1 MiB DRAM
 #define DRAM_BASE 0x80000000
